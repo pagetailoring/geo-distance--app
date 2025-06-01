58cc11a290c1e3bc4c22a41292cb9a171c6b5685
Page Tailoring <bartek@pagetailoring.com>
2025-06-01 23:16:43 +0200

    chore: basic environment configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  files: ['**/*.ts', '**/*.tsx', '**/*.vue'],
+  rules: {
+    'no-console': 'off',
+  },
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-05-15",
+  modules: ['@nuxtjs/html-validator', '@nuxt/eslint', '@nuxt/fonts', '@vueuse/nuxt'],
   devtools: { enabled: true },
 
-  modules: ["@nuxt/eslint", "@nuxt/fonts"],
-});
+  css: ['~/assets/styles/main.scss'],
+  compatibilityDate: '2025-05-15',
+
+  typescript: { typeCheck: true, strict: true },
+
+  eslint: { config: { stylistic: true } },
+})

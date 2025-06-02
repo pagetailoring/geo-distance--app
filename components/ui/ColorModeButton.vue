585b0013e87c780c8423be70a8643812a180f135
Page Tailoring <bartek@pagetailoring.com>
2025-06-02 15:38:56 +0200

    feat: add ColorModeButton component for dark/light mode toggle

---
+++ components/ui/ColorModeButton.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+
+const { shift, c } = useMagicKeys()
+whenever(c, () => (shift.value ? toggleDark() : null))
+</script>
+
+<template>
+  <ClientOnly>
+    <UiButton class="mode" @click="toggleDark()">
+      {{ isDark ? 'dark' : 'light' }}
+    </UiButton>
+  </ClientOnly>
+</template>
